5b8c1273d51019b975ce988ee2f1a522e54d153e
LIKLY1 <louismottier123@gmail.com>
2025-09-12 00:00:13 +0200

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,22 +5,3 @@ export default {
   },
   plugins: [],
 }
-// tailwind.config.js
-module.exports = {
-  darkMode: 'class', // 'class' ou 'media' selon ce que tu veux ; 'class' est généralement plus sûr
-  content: [
-    './src/**/*.{js,jsx,ts,tsx,html}',
-    './public/index.html'
-  ],
-  theme: {
-    extend: {}
-  },
-  safelist: [
-    // classes dynamiques que Tailwind pourrait purger ; ajoute ici si tu utilises des classes construites dynamiquement
-    'text-emerald-600','text-emerald-400',
-    'text-red-600','text-red-400',
-    'text-zinc-50','text-zinc-900',
-    'bg-zinc-950','bg-white'
-  ],
-  plugins: []
-};
