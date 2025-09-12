e7ee4ab0d911df97861ea76505000f16ceb7cbed
LIKLY1 <louismottier123@gmail.com>
2025-09-12 20:09:09 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,6 @@ import { supabase } from "./supabaseClient";
 import Papa from "papaparse";
 import AuthPage from "./AuthPage";
 import logo from "./logo.png";
-import { Analytics } from "@vercel/analytics/next";
 
 
 // --- Constants / Types ---
